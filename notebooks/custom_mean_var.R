0592804869e62e9df2d79630f0f7b701fd575cc2
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-04-02 01:05:22 -0400

    made the code more efficient

--- notebooks/custom_mean_var.R
+++ notebooks/custom_mean_var.R
@@ -1,6 +1,7 @@
 library(Rcpp)
 library(foreach)
 library(doMC)
+library(rbenchmark)
 sourceCpp("notebooks/custom_mean_var.cpp")
 
 distributed.var <- function(x, n.splits = 16, n.cores = 2) {
@@ -22,17 +23,18 @@ distributed.var <- function(x, n.splits = 16, n.cores = 2) {
 
   mns <- unlist(lapply(res, function(x) x[1]))
   vars <- unlist(lapply(res, function(x) x[2]))
-  return(combine_stats(mns, vars, ns))
+  return(combine_stats_var(mns, vars, ns))
 }
 
 x <- c(runif(10^8, 0, 1))
+y <- 0.1 * x + c(runif(10^8, 0, 1))
 
 rbenchmark::benchmark(custom_mean_var(x), replications = 10)
 rbenchmark::benchmark(distributed.var(x, n.splits = 32, n.cores = 4), replications = 10)
 rbenchmark::benchmark(c(mean(x), sd(x)^2), replications = 10)
 
 a <- custom_mean_var(x)
-b <- distributed.var(x, n.splits = 32, n.cores = 4)
+b <- distributed.var(x, n.splits = 2, n.cores = 2)
 c <- c(mean(x), var(x))
 
 err.R.internal.var.wrt.custom.mean.var <- (c - a)/a
--- notebooks/custom_mean_var.cpp
+++ notebooks/custom_mean_var.cpp
@@ -3,27 +3,15 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericVector distributed_var_base(NumericVector v1, NumericVector v2, int n_A, int n_B) {  
-  double delta = v2[0] - v1[0];
-  double m_a = v1[1] * (n_A - 1);
-  double m_b = v2[1] * (n_B - 1);
-  double M2 = m_a + m_b + delta * delta * n_A * n_B / (n_A + n_B);
-  NumericVector res;
-  res.push_back((v1[0]*n_A + v2[0]*n_B)/(n_A + n_B));
-  res.push_back(M2/(n_A + n_B - 1));
-  return res;
-}
-
-// [[Rcpp::export]]
 NumericVector custom_mean_var(NumericVector x) { 
   NumericVector y;
-
+  double* x1 = &x[0];
   int n = 0;
   double mn = 0;
   double M2 = 0;
   
   for(int i = 0; i < x.size() ; i++) {
-    double xi = x[i];
+    double xi = x1[i];
     n++;
     double delta = xi - mn;
     mn += delta/n;
@@ -32,7 +20,7 @@ NumericVector custom_mean_var(NumericVector x) {
   }
   
   if (n < 2) {
-    y.push_back(x[0]);
+    y.push_back(x1[0]);
     y.push_back(0);
   } else {
     y.push_back(mn);
@@ -42,59 +30,43 @@ NumericVector custom_mean_var(NumericVector x) {
   return y;
 }
 
-// [[Rcpp::export]]
-NumericVector combine_stats(NumericVector mns, NumericVector vars, NumericVector ns) {
-  if (mns.size() == 2) {
-    NumericVector res;
-    NumericVector v1;
-    NumericVector v2;
-    v1.push_back(mns[0]);
-    v1.push_back(vars[0]);
-    v2.push_back(mns[1]);
-    v2.push_back(vars[1]);
-    
-    return(distributed_var_base(v1, v2, ns[0], ns[1]));
+double* distributed_var_base(double mn1, double mn2, double var1, double var2, int n_A, int n_B) {  
+  double delta = mn1 - mn2;
+  double m_a = var1 * (n_A - 1);
+  double m_b = var2 * (n_B - 1);
+  double M2 = m_a + m_b + delta * delta * n_A * n_B / (n_A + n_B);
+  double* res = new double[2];
+  res[0] = ((mn1*n_A + mn2*n_B)/(n_A + n_B));
+  res[1] = (M2/(n_A + n_B - 1));
+  return res;
+}
+
+double* combine_stats_var_internal(double* mns, double* vars, double* ns, int mns_len, int vars_len, int ns_len) {
+  if (mns_len == 2) {
+    return(distributed_var_base(mns[0], mns[1], vars[0], vars[1], ns[0], ns[1]));
   }
   
-  NumericVector mns_left;
-  for (int i = 0; i < mns.size()/2 ; i++) {
-    mns_left.push_back(mns[i]);
-  }
-  NumericVector mns_right;
-  for (int i = mns.size()/2; i < mns.size() ; i++) {
-    mns_right.push_back(mns[i]);
-  }
-
-  NumericVector vars_left;
-  for (int i = 0; i < vars.size()/2 ; i++) {
-    vars_left.push_back(vars[i]);
-  }
-  NumericVector vars_right;
-  for (int i = vars.size()/2; i < vars.size() ; i++) {
-    vars_right.push_back(vars[i]);
-  }
-
-  NumericVector ns_left;
-  for (int i = 0; i < ns.size()/2 ; i++) {
-    ns_left.push_back(ns[i]);
-  }
-  NumericVector ns_right;
-  for (int i = ns.size()/2; i < ns.size() ; i++) {
-    ns_right.push_back(ns[i]);
-  }  
-  
-  NumericVector v1 = combine_stats(mns_left, vars_left, ns_left);
-  NumericVector v2 = combine_stats(mns_right, vars_right, ns_right);
+  double* v1 = combine_stats_var_internal(mns, vars, ns, mns_len/2, vars_len/2, ns_len/2);
+  double* v2 = combine_stats_var_internal(mns + mns_len/2, vars + vars_len/2, ns + ns_len/2, mns_len/2, vars_len/2, ns_len/2);
   
   int ns_sum_left = 0;
-  for (int i = 0; i < ns_left.size() ; i++) {
-    ns_sum_left += ns_left[i];
+  for (int i = 0; i < ns_len/2 ; i++) {
+    ns_sum_left += ns[i];
   }
   int ns_sum_right = 0;
-  for (int i = 0; i < ns_right.size() ; i++) {
-    ns_sum_right += ns_right[i];
+  for (int i = ns_len/2; i < ns_len ; i++) {
+    ns_sum_right += ns[i];
   }
   
-  NumericVector res = distributed_var_base(v1, v2, ns_sum_left, ns_sum_right);
+  double* res = distributed_var_base(v1[0], v2[0], v1[1], v2[1], ns_sum_left, ns_sum_right);
+  return res;
+}
+
+// [[Rcpp::export]]
+NumericVector combine_stats_var(NumericVector mns, NumericVector vars, NumericVector ns) {
+  double* d = (combine_stats_var_internal(&mns[0], &vars[0], &ns[0], mns.size(), vars.size(), ns.size()));
+  NumericVector res;
+  res.push_back(d[0]);
+  res.push_back(d[1]);
   return res;
 }
