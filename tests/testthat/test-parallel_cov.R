584c975e37b6f60ab43351b3465dab5e8a6646b6
Shantanu Singh <shsingh@broadinstitute.org>
2017-04-02 18:39:42 -0400

    Added tests

---
+++ tests/testthat/test-parallel_cov.R
@@ -0,0 +1,9 @@
+context("`parallel_cov` computes covariance")
+
+test_that("The result of `parallel_cov` is nearly the same as `cov` ", {
+  x <- matrix(rnorm(1000), 100, 10)
+  
+  expect_equal(cytominergallery::parallel_cov(x),
+               cov(x))
+
+})
