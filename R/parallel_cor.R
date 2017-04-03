af937253e91ff941099ed992832bf1bad1ca16dc
Shantanu Singh <shsingh@broadinstitute.org>
2017-04-03 10:42:17 -0400

    Refactored R code

--- R/parallel_cor.R
+++ R/parallel_cor.R
@@ -1,6 +1,6 @@
-#' Parallel correlation function
+#' Compute correlation function in batches, in parallel
 #'
-#' @param x ...
+#' @param x data matrix 
 #' @param splits shows the number of splits of data that we want to combine  (should be of 2^n form)
 #' @param cores number of cores that will be used for the parallel computation
 #' @param cov_fun covariance function used on each split
@@ -13,15 +13,13 @@
 #'
 parallel_cor <- function(x, splits = 2, cores = 2, cov_fun = "two_pass_multi_covar") {
   
-  cov_mat <- parallel_cov(x, splits = splits, cores = cores, cov_fun = cov_fun)
+  cov_mat <-as.matrix(parallel_cov(x, splits = splits, cores = cores, cov_fun = cov_fun))
   
-  nrm <- diag(diag(cov_mat)^-0.5)
+  # \cor(X, Y) = \frac{\cov(X, Y)}{\sd(X) \sd(Y)}
   
-  cov_mat <- cov_mat %>% as.matrix()
+  inv_sd <- as.matrix(diag(diag(cov_mat)^-0.5))
   
-  nrm <- as.matrix(nrm)
-  
-  result <- nrm %*% cov_mat %*% nrm
+  result <- inv_sd %*% cov_mat %*% inv_sd
   
   rownames(result) <- colnames(x)
   
--- R/parallel_cov.R
+++ R/parallel_cov.R
@@ -1,6 +1,6 @@
-#' Parallel covariance function
+#' Compute covariance function in batches, in parallel
 #'
-#' @param x ...
+#' @param x data matrix 
 #' @param splits shows the number of splits of data that we want to combine  (should be of 2^n form)
 #' @param cores number of cores that will be used for the parallel computation
 #' @param cov_fun covariance function used on each split
@@ -19,35 +19,35 @@ parallel_cov <- function(x, splits = 2, cores = 2, cov_fun = "two_pass_multi_cov
 
   doParallel::registerDoParallel(cores = cores)
   
-  ns <- rep(round(n/splits), splits - 1)
+  batches <- rep(round(n/splits), splits - 1)
 
-  ns <- c(ns, n - sum(ns))
+  batches <- c(batches, n - sum(batches))
 
   j <- 0 # to avoid warning: no visible binding for global variable ‘j’
   
-  result <- foreach::foreach(j = 1:length(ns)) %dopar% {
+  result <- foreach::foreach(j = 1:length(batches)) %dopar% {
     if (j == 1) {
       i <- 1
 
     } else {
-      i <- sum(ns[1:(j-1)]) + 1
+      i <- sum(batches[1:(j-1)]) + 1
 
     }
     
-    k <- sum(ns[1:j])
+    k <- sum(batches[1:j])
 
-    mn <- apply(x[i:k,], 2, mean)
+    batch_mean <- apply(x[i:k,], 2, mean)
 
-    s <- do.call(cov_fun, list(x[i:k, ]))
+    batch_cov <- do.call(cov_fun, list(x[i:k, ]))
 
-    rbind(mn, s)
+    rbind(batch_mean, batch_cov)
   }
 
-  u <- do.call(cbind, result)
+  batch_mean_cov <- do.call(cbind, result)
 
-  mn.cr <- combine_cov_estimates(u, ns)
+  mean_cov <- combine_cov_estimates(batch_mean_cov, batches)
 
-  result <- mn.cr[2:NROW(mn.cr), ]
+  result <- mean_cov[2:NROW(mean_cov), ]
 
   rownames(result) <- colnames(x)
 
